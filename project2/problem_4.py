131d500768189dd33d2128ff2bbdc695d60d7971
Yeray García Quintana <64692593+y-garcia@users.noreply.github.com>
2023-11-04 11:03:26 +0100

    feat: Add tests for problem 4 of project 2

--- project2/problem_4.py
+++ project2/problem_4.py
@@ -28,16 +28,16 @@ def is_user_in_group(user, group):
       user(str): user name/id
       group(class:Group): group to check user membership against
     """
+    if user is None or group is None:
+        return None
+
     if user in group.get_users():
-        print(f"User '{user}' is in group '{group.get_name()}'")
         return True
 
     for child_group in group.get_groups():
         if is_user_in_group(user, child_group):
-            print(f"User '{user}' is in group '{group.get_name()}'")
             return True
 
-    print(f"User '{user}' is not in group '{group.get_name()}'")
     return False
 
 
@@ -46,6 +46,8 @@ def is_user_in_group(user, group):
 
 
 # Test Case 1
+print("\n1. Test main functionality")
+
 parent = Group("parent")
 child = Group("child")
 sub_child = Group("sub_child")
@@ -61,17 +63,42 @@ parent.add_user(parent_user)
 child.add_user(child_user)
 sub_child.add_user(sub_child_user)
 
+print("User 'parent_user':")
 assert is_user_in_group(parent_user, parent) is True
+print("  - is in group 'parent'")
 assert is_user_in_group(parent_user, child) is False
+print("  - is not in group 'child'")
 assert is_user_in_group(parent_user, sub_child) is False
+print("  - is not in group 'sub_child'")
 
+print("User 'child_user':")
 assert is_user_in_group(child_user, parent) is True
+print("  - is in group 'parent'")
 assert is_user_in_group(child_user, child) is True
+print("  - is in group 'child'")
 assert is_user_in_group(child_user, sub_child) is False
+print("  - is not in group 'sub_child'")
 
+print("User 'sub_child_user':")
 assert is_user_in_group(sub_child_user, parent) is True
+print("  - is in group 'parent'")
 assert is_user_in_group(sub_child_user, child) is True
+print("  - is in group 'child'")
 assert is_user_in_group(sub_child_user, sub_child) is True
+print("  - is in group 'sub_child'")
+
 # Test Case 2
+print("\n2. Return None if user or group is None")
+
+assert is_user_in_group(None, parent) is None
+print("is_user_in_group(None, parent) ==", is_user_in_group(None, parent))
+
+assert is_user_in_group(parent_user, None) is None
+print("is_user_in_group(parent_user, None) ==", is_user_in_group(parent_user, None))
 
 # Test Case 3
+print("\n2. Return False if group is empty")
+
+empty_group = Group("empty_group")
+assert is_user_in_group("any_user", empty_group) is False
+print("is_user_in_group('any_user', empty_group) == ", is_user_in_group("any_user", empty_group))
